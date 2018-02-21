6c047f7d403a942016bcd8ad4803f6b102a5398b
Aluno Fiar Logatti <daianecastagner@gmail.com>
2018-02-21 20:45:05 -0300

    teste

--- src/br/com/proj/entidade/Moto.java
+++ src/br/com/proj/entidade/Moto.java
@@ -1,5 +1,31 @@
 package br.com.proj.entidade;
 
 public class Moto {
+	
+	private int moto;
+	private String descricao;
+	private int ano;
+	public int getMoto() {
+		return moto;
+	}
+	public void setMoto(int moto) {
+		this.moto = moto;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	
+	
+	
 
 }
