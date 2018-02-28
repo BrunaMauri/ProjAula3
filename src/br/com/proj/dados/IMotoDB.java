10c6725d28bc1ce4473b88b6c5bdff3b47523e74
adenilsonbc <adenilsonc7@gmail.com>
2018-02-28 19:43:20 -0300

    Atualização de página

--- src/br/com/proj/dados/IMotoDB.java
+++ src/br/com/proj/dados/IMotoDB.java
@@ -1,5 +1,17 @@
 package br.com.proj.dados;
 
-public interface IMotoDB {
+import java.util.List;
+import br.com.proj.entidade.Moto;
 
+public interface IMotoDB {
+	
+	
+	boolean insert(Moto moto);
+	boolean update(Moto moto);
+	boolean delete(Moto moto);
+	List<Moto> select ();	
 }
+
+// CTRL + SHIFT + O = IMPORTE DE DEPENDÊNCIAS.
+// CTRL + SHIFT + F = IDENTAR O CÓDIGO.
+
--- src/br/com/proj/dados/MotoDB.java
+++ src/br/com/proj/dados/MotoDB.java
@@ -1,5 +1,41 @@
 package br.com.proj.dados;
 
-public class MotoDB {
+import java.util.List;
+
+import br.com.proj.entidade.Moto;
+
+public class MotoDB implements IMotoDB{
+	
+	
+	public boolean insert (Moto moto){
+		
+		System.out.println("Id: " +moto.getId());
+		System.out.println(" Descrição: "+ moto.getDescricao());
+		System.out.println("Ano: "+moto.getAno());
+		
+		//IMPLEMENTAÇÃO DO INSERT NA BASE DE DADOS
+		
+		return true;
+	}
+
+	@Override
+	public boolean update(Moto moto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Moto moto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Moto> select() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
