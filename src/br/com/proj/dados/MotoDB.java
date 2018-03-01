bece16a6a945586b18b47490c564c7f083905ef4
adenilsonbc <adenilsonc7@gmail.com>
2018-02-28 21:37:19 -0300

    Atualização

--- src/br/com/proj/dados/MotoDB.java
+++ src/br/com/proj/dados/MotoDB.java
@@ -1,21 +1,66 @@
 package br.com.proj.dados;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
-import br.com.proj.entidade.Moto;
+import javax.management.monitor.StringMonitor;
 
-public class MotoDB implements IMotoDB{
-	
-	
-	public boolean insert (Moto moto){
+import com.mysql.jdbc.PreparedStatement;
+
+import br.com.proj.entidade.Moto;
+import br.com.proj.util.Conexao;
+
+public class MotoDB implements IMotoDB {
+
+	private Connection con;
+	private java.sql.PreparedStatement ps;
+
+	public MotoDB() {
+		con = Conexao.getConnection();
+	}
+
+	public boolean insert(Moto moto) {
+
+		System.out.println("Id: " + moto.getId());
+		System.out.println(" Descrição: " + moto.getDescricao());
+		System.out.println("Ano: " + moto.getAno());
+
+		// IMPLEMENTAÇÃO DO INSERT NA BASE DE DADOS
+
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(" INSERT INTO MOTO ");
+
+		sb.append(" (id, descricao, ano) values (?, ?, ?) ");
+
+		try {
+			ps = this.con.prepareStatement(sb.toString());
+
+			ps.setInt(1, moto.getId());
+			ps.setString(2, moto.getDescricao());
+			ps.setInt(3, moto.getAno());
+			ps.execute();
+			
+			return true;
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return false;
 		
-		System.out.println("Id: " +moto.getId());
-		System.out.println(" Descrição: "+ moto.getDescricao());
-		System.out.println("Ano: "+moto.getAno());
-		
-		//IMPLEMENTAÇÃO DO INSERT NA BASE DE DADOS
-		
-		return true;
+		// PreparedStatement stmt = this.con.prepareStatement("INSERT INTO moto" +
+
 	}
 
 	@Override
@@ -35,7 +80,5 @@ public class MotoDB implements IMotoDB{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
 
 }
